7444a3b6b02a2d75b302553bea987ef7315dd27f
alexejbat <alexejbat@yandex.ru>
2021-09-22 13:41:33 +0400

    refresh

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+const elementLi = React.createElement('li', {className: 'li'}, ' element')
+
+const element = React.createElement('ul', {className: 'ul'}, elementLi)
+
+
+ReactDOM.render(element, document.getElementById('root'));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
